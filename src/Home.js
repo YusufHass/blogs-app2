d71e1902679a0d1462f105c87e57ded119533fea
YusufHass <judekal82@gmail.com>
2022-02-08 23:37:59 -0500

    dynamic changes with useState

--- src/Home.js
+++ src/Home.js
@@ -2,11 +2,17 @@ import React, { useState } from "react";
 
 const Home = () => {
     let name1= 'Alex1'
+    let age1= 25
     const [name,setName]= useState('Alex')
+    const [age,setAge]= useState(25)
+
 
 const changeName=()=>{
     setName('Yesuf')
-    let name1= 'Alex1'
+    name1= 'Alex1'
+    age1= 35
+
+    setAge(30)
 }
   const handleClick = () => {
     console.log("Hello, ninja");
@@ -22,10 +28,14 @@ const changeName=()=>{
   return (
     <div className="home">
       <h1>Home Page</h1>
-     <h1 style={{color: 'orange'}}> {name1}</h1>
-     <h1 style={{color: 'brown'}}> {name}</h1>
+     <h1 style={{color: 'orange'}}> {name1} is {age1} old</h1>
+     <h1 style={{color: 'brown'}}> {name} is {age} old</h1>
       <div></div>
       <button onClick={changeName}>Click me to change names</button>
+      {/* The following makes with handleClick() automatically invoke without click
+      so we pass without () so it invokes after click */}
+      {/* <button onClick={handleClick()}>Click me</button> */}
+
       <button onClick={handleClick}>Click me</button>
 
       <button onClick={(e) => handleButton2("Sarah", e)}>Button 2</button>
